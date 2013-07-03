e688d2e7942602b87a20e1d0a77045e80c2baf33
Kenneth Fung <kenf130@gmail.com>
2013-07-03 19:41:42 +0000

    update prime generator javascript

--- hw1p2.js
+++ hw1p2.js
@@ -1,6 +1,19 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var outfile = "hello.txt";
-var out = "A startup is a business built to grow rapidly.\n";
-fs.writeFileSync(outfile, out);
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+var outfile = "primes.txt";
+var primes = new Array();
+var n = 2;  
+while (primes.length < 100) {
+  var factor = false;
+  for (var i in primes) { 
+    if (n % primes[i] == 0) {
+      factor = true;
+      break;
+    }
+  }
+  if (factor == false) {
+    primes.push(n);
+  }
+  n++;
+}
+fs.writeFileSync(outfile, primes);
